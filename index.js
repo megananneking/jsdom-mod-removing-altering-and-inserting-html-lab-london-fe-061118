9c847fc278bfb19a67116c0b7c1ed1f189d48b9e
megananneking <megan.king@wework.com>
2018-07-09 18:44:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 document.querySelector('main').remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = document.createElement('h1#victory')
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
 document.body.appendChild(newHeader)
 newHeader.innerHTML = 'Megan is the champion!'
