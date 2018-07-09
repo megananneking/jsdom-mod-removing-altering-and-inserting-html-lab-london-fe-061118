2707a7eb295ce9543610ccf945a76daf8c398911
megananneking <megan.king@wework.com>
2018-07-09 18:25:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 /* Enter the code to remove the main node element under this comment */
-<h1 id="victory"></h1>
+
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
