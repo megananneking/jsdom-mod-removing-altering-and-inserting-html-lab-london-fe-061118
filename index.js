494579747188f3bd4c52aa42fac9b1fd7d5987ec
megananneking <megan.king@wework.com>
2018-07-09 18:29:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
+document.body.appendChild(element)
 var element = document.createElement('h1')
